f648b3223cf5de5fb565420d6caf8299fb6591ec
Nova <novalailatulrizkiyah@gmail.com>
2022-01-13 13:36:50 +0700

    kedua

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Layout.module.css";
+
+export default function Layout({children})
+{
+    return (
+        <> 
+        <p> 
+            Example of layout
+        </p> {children}
+    </>
+
+    )
+       
+    
+}
