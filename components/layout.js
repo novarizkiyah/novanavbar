30551416f09e52690592c87552df5bdce1b301c4
Nova <novalailatulrizkiyah@gmail.com>
2022-01-15 20:48:33 +0700

    tanggal 15 jan bosq

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,49 @@
+import { useState } from "react";
 import styles from "../styles/Layout.module.css";
+import Link from "next/link";
 
-export default function Layout({children})
-{
-    return (
-        <> 
-        <p> 
-            Example of layout
-        </p> {children}
+export default function Layout({children}){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const openmenu =  ()=>setIsOpen(!isOpen);
+    return <>
+
+        <header className={styles.header}>
+            <nav className={styles.navbar}>
+                <Link href="/">
+                    <a className={styles.navlogo}>studywithnova</a>
+                </Link>
+                
+            <ul className={isOpen === false ? styles.navmenu : styles.navmenu + ' ' + styles.active}>
+                <li className={styles.navitem}>
+                    <Link href="/">
+                        <a className={styles.navlink}>Home</a>
+                    </Link>
+                    
+                </li>
+                <li className={styles.navitem}>
+                
+                    <a className={styles.navlink}>About</a>
+        
+                </li>
+                <li className={styles.navitem}>
+            
+                    <a className={styles.navlink}>Contact</a>
+        
+                </li>
+            </ul>
+            <button className={isOpen === false ? 
+                styles.hamburger : styles.hamburger + ' ' + styles.active}
+                onClick={openmenu}
+                >
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </button>
+            </nav>
+        </header>
+         
+        {children}
+        {/*footer*/}
     </>
-
-    )
-       
-    
 }
